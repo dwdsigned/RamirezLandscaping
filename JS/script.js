63252f20a11807aa5d97a5c883ffee2ed0aac895
Darius Williams <dariustwilliams668@gmail.com>
2023-11-14 17:01:52 -0500

    JS start

---
+++ JS/script.js
@@ -0,0 +1,27 @@
+// Img Element
+const portImg = document.querySelector("#portImg");
+console.log(portImg);
+
+// Before button
+const before = document.querySelector("#before-btn");
+console.log(before);
+
+// After button
+const after = document.querySelector("#after-btn");
+console.log(after);
+
+// Left arrow button
+const leftArrow = document.querySelector("#portLeft");
+console.log(leftArrow);
+
+// Right arrow button
+const rightArrow = document.querySelector("#portRight");
+console.log(rightArrow);
+
+// Images
+const images = [
+    {
+        
+    }
+]
+
